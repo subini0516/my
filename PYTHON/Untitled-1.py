32e7d163753fa80fe2be3d2a53800055586c46f5
Your Name <you@example.com>
2024-06-12 10:39:03 +0900

    11

---
+++ PYTHON/Untitled-1.py
@@ -0,0 +1,607 @@
+#height=float(input('키 (cm) 입력:'))
+#weight=(height-100)*0.9
+#print('표준체중은',weight,'입니다')
+
+
+
+
+#x=10
+#if x>1:
+#    print('x는 1보다 크다')
+#print('이것도 실행된다')
+
+#num=int(input('정수입력:'))
+#if num%10:
+#    print('10의 배수가 아닙니다.')
+
+#num=int(input('정수입력:'))
+#if num<100:
+#    print('입력한 정수',num,'는 100보다 작음')
+#print('프로그램 종료')
+
+
+
+
+#a=10
+#if a==10:
+#    print('10입니다')
+#else:
+#    print('10이 아닙니다')
+
+
+
+
+#num=int(input('정수입력:'))
+#if num%2==0:
+#    print('짝수입니다')
+#else:
+#    print('홀수입니다')
+#print('\n프로그램 종료')
+
+
+
+
+#print('두 개의 정수를 입력해주세요')
+#num1=int(input('정수1 입력:'))
+#num2=int(input('정수2 입력:'))
+#if num1>num2:
+#    ans=num1
+#else:
+#    ans=num2
+#print('둘 중 큰 수는',ans,'입니다')
+
+
+
+
+
+
+#print('두 개의 정수를 입력해주세요')
+#num1=int(input('정수1 입력:'))
+#num2=int(input('정수2 입력:'))
+#if num1>num2:
+#    ans=num1-num2
+#else:
+#    ans=num2-num1
+#print('두 수의 차는',ans,'입니다')
+
+
+
+
+
+
+
+#a=-9
+#if a>0:
+#    if a==10:
+#        print('10입니다')
+#    else:
+#        print('10이 아닙니다')
+#else:
+#    print('0보다 작습니다')
+
+
+
+
+
+
+#num=int(input('정수입력:'))
+#if num>=0:
+#    print('양수 입니다')
+#    if num%2==0:
+#        print('짝수 입니다')
+#    else:
+#        print('홀수 입니다')
+#else:
+#    print('음수 입니다')
+
+
+
+
+
+#age=int(input('나이 입력:'))
+#if age>=20:
+#    print('응시가능')
+#    score=int(input('점수 입력:'))
+#    if score>=80:
+#        print('합격')
+#    else:
+#        print('불합격')
+#else:
+#    print('응시 불가능')
+
+
+
+
+
+#print('*'*45)
+#print('성별과 몸무게, 키에 따른 운동경기 추천 프로그램')
+
+#gender=input('성별 입력[남자/여자]:')
+
+#if gender=='남자':
+#    weight=float(input('몸무게 입력(kg):'))
+#    if weight>=70:
+#        print('[농구] 경기를 추천합니다')
+#    else:
+#        print('[축구] 경기를 추천합니다')
+#else:
+#    height=float(input('키 입력(cm):'))
+#    if height>=160:
+#        print('[배구] 경기를 추천합니다')
+#    else:
+#        print('[피구] 경기를 추천합니다')
+
+
+
+
+
+#a=10
+#if a==10:
+#    print('10입니다')
+#elif a==9:
+#    print('9입니다')
+#else:
+#    print('10,9 모두 아닙니다')
+
+
+
+
+
+#score=int(input('정수 입력:'))
+#if score>=70:
+#    print('합격')
+#elif score>=50:
+#    print('재시')
+#else:
+#    print('불합격')
+
+
+
+
+
+
+#score=int(input('점수 입력:'))
+#if score>=90:
+#    print('a학점')
+#elif score>=80:
+#    print('b학점')
+#elif score>=70:
+#    print('c학점')
+#elif score>=60:
+#    print('d학점')
+#else:
+#    print('f학점')
+
+
+
+
+
+
+#year=int(input('연도를 입력하시오:'))
+#if ((year%4==0 and year%100!=0) or year%400==0):
+#    print(year,'년은 윤년입니다')
+#else:
+#    print(year,'년은 평년입니다')
+
+
+
+
+
+#score=int(input('정수 입력:'))
+#if score>=70:
+#    pass
+#elif score>=50:
+#    pass
+#else:
+#    pass
+
+
+
+
+
+#for i in range(2):
+#    print('hello world!!')
+
+
+
+
+
+#dan=int(input('출력하고 싶은 단:'))
+#i=1
+#while i<10:
+#    print(dan,'*',i,'=',dan*i)
+#    i+=1
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#i=1
+#while i<=num:
+#    print(i,end=" ")
+#    i+=1
+
+
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#while num>=0:
+#    print(num,end=" ")
+#    num-=1
+
+
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#i=2
+#while i<=num:
+#    print(i,end=" ")
+#    i+=2
+
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#i=0
+#cnt=0
+#while cnt<num:
+#    cnt+=1
+#    i+=5
+#    print(i)
+
+
+
+
+
+
+
+
+
+
+#i=0
+#while i<3:
+#    print('Hello World!!')
+#    i=i+1
+
+
+
+
+
+
+
+
+
+#i=5
+#while i<13:
+#    print(i,end=" ")
+#    i+=1
+
+
+
+
+
+
+#dan=int(input('출력하고 싶은 단:'))
+#i=1
+#while i<10:
+#    print(dan,'*',i,'=',dan*i)
+#    i+=1
+
+
+
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#while num>=0:
+#    print(num,end=" ")
+#    num-=1
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#i=2
+#while i<=num:
+#    print(i,end=" ")
+#    i*=2
+
+
+
+
+
+
+#num=int(input('양의 정수 입력:'))
+#i=0
+#cnt=0
+#while cnt<num:
+#    cnt+=1
+#    i+=3
+#    print(i)
+
+
+
+
+
+
+
+
+#num1=int(input('정수입력:'))
+#num2=int(input('정수입력:'))
+#if num1>num2:
+#    upper=num1
+#    lower=num2
+#else:
+#    upper=num2
+#    lower=num1
+#i=upper
+#while i>=lower:
+#    print(i)
+#    i-=1
+
+
+
+
+
+
+
+
+#n=int(input('양의 정수 입력:'))
+#i=1
+#sum=0
+#while i<=n:
+#    sum=sum+i
+#    i+=1
+#print('1부터',n,'까지의 합은',sum,'입니다')
+
+
+
+
+
+
+
+
+#i=0
+#
+#while i<3:
+#    n=int(input('정수입력:'))
+#    sum=sum+n
+#    i+=1
+#print('합계는',sum,'입니다')
+
+
+
+
+
+
+
+
+#sum=0
+#n=int(input('정수입력:'))
+#while n>0:
+#    digit=n%10
+#    sum=sum+digit
+#    n=n//10
+#print('합계는',sum,'입니다')
+
+
+
+
+
+
+#n=int(input('정수입력:'))
+#count=0
+#temp=n
+#while temp>0:
+#    temp=temp//10
+#    count+=1
+#print(n,'은',count,'자리 숫자 입니다')
+
+
+
+
+
+
+#n=0
+#sum=0
+#score=0
+#print('종료하려면 음수를 입력하세요')
+#while score>=0:
+#    score=int(input('성적입력:'))
+#    if score>0:
+#        sum=sum+score
+#        n=n+1
+#if n>0:
+#    avg=sum/n
+#print('성적의 평균은 %f입니다' %avg)
+
+
+
+
+
+#max=-1
+#n=int(input('양의 정수 입력:'))
+#while n>=0:
+#    if n>max:
+#        max=n
+#    n=int(input('양의 정수 입력:'))
+#if max!=-1:
+#    print('최댓값은',max,'입니다')
+
+
+
+
+
+
+
+
+#import random
+#num=random.randint(6,100)
+#print(num)
+
+
+
+
+
+
+
+#import random
+#num=0
+#while num!=3:
+#    num=random.randint(1,6)
+#    print('주사위를 굴렸다',num,'나옴')
+#print('드디어 3이 나와서 종료합니다')
+
+
+
+
+
+
+
+
+#import random
+#print('구구단 게임입니다')
+#i=0
+#score=0
+#while i<3:
+#    num1=random.randint(1,9)
+#    num2=random.randint(1,9)
+#    result=num1*num2
+#    print(num1,'*',num2,'=',end='')
+#    ans=int(input())
+#    if ans==result:
+#        print('맞습니다')
+#        score+=1
+#    else:
+#        print('틀렸습니다')
+#    i+=1
+#print('\n총 3문제 중',score,'문제 맞추셨습니다')
+
+
+
+
+
+
+
+
+
+#import random
+#print('이길 때까지 계속 합니다')
+#win=False
+#while win!=True:
+#    ans=input('가위,바위,보 중 하나를 선택: ')
+#    com=random.randint(1,3)
+#    print('컴퓨터:',end='')
+
+    #1-가위
+#    if com==1:
+#        print('가위')
+#        if ans=='가위':
+#            print('비겼다')
+#        elif ans=='바위':
+#            print('이겼다')
+#            win=True
+#       elif ans=='보':
+#            print('졌다')
+
+    #2-바위
+#    elif com==2:
+#        print('바위')
+#        if ans=='가위':
+#            print('졌다')
+#        elif ans=='바위':
+#            print('비겼다')
+#        elif ans=='보':
+#            print('이겼다')
+#            win=True
+
+    #3-보
+#    elif com==3:
+#        print('보')
+#        if ans=='가위':
+#            print('이겼다')
+#            win=True
+#        elif ans=='바위':
+#            print('졌다')
+#        elif ans=='보':
+#           print('비겼다')
+
+
+
+
+
+
+
+#i=4
+#while i<10:
+#    print(i,end=" ")
+#    i+=5
+            
+
+
+
+
+
+#num = int(input("양의 정수 입력: "))
+#i = 2
+#while i <= num :
+#    print(i , end=" ")
+#    i **= 2
+
+
+
+
+
+
+
+
+
+
+#s=[100, 90, 80, 70, 60]
+#sum=sum(s)
+#print('성적합계:', sum)
+#print('성적평균:', sum/len(s))
+
+
+
+
+
+
+
+
+
+score=[]
+value=0
+print('종료하려면 음수를 입력하세용')
+
+while value>=0:
+    value=int(input('성적입력:'))
+    if value>=0:
+        score.append(value)
+
+if len(score)>0:
+    sum=sum(score)
+    min=min(score)
+    max=max(score)
+
+    print('최고 점수:', max)
+    print('최저 점수:', min)
+    print('평균 점수:', sum/len(score))
